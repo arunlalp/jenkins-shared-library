681747ff850a609aef672a7254d003d93d23dc81
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-23 14:11:02 +0530

    Update ecrPush.groovy

--- vars/ecrPush.groovy
+++ vars/ecrPush.groovy
@@ -1,1 +1,5 @@
-
+def call() {
+  sh 'aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/c1l9p0p2'
+  sh "docker tag arun_custom_repo:latest public.ecr.aws/c1l9p0p2/arun_custom_repo:latest"
+  sh "docker push public.ecr.aws/c1l9p0p2/arun_custom_repo:latest"
+}
