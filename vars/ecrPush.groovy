61c839fd3c19a9c621bfa141114276866c0edf89
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-28 10:11:02 +0530

    Update ecrPush.groovy

--- vars/ecrPush.groovy
+++ vars/ecrPush.groovy
@@ -1,5 +1,5 @@
 def call() {
-  sh 'aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/c1l9p0p2'
-  sh "docker tag arun_custom_repo:latest public.ecr.aws/c1l9p0p2/arun_custom_repo:latest"
-  sh "docker push public.ecr.aws/c1l9p0p2/arun_custom_repo:latest"
+  sh 'aws ecr-public get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_URL}'
+  sh 'docker tag ${DOCKER_IMAGE_TAG} ${ECR_IMAGE_TAG}'
+  sh 'docker push ${ECR_IMAGE_TAG}'
 }
