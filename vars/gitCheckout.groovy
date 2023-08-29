033e240d10a918fa4b1e36ea311ab3a471a4094d
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-29 15:22:34 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,8 @@
-def call() {
-  git branch: 'main', changelog: false, poll: false, url: 'https://github.com/arunlalp/java_code.git'
-}
+def call(Map stageParams) {
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
