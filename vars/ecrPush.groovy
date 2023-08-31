1301be4c7310f49d9d854cf9afc990a23deca531
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-31 11:46:48 +0530

    Update ecrPush.groovy

--- vars/ecrPush.groovy
+++ vars/ecrPush.groovy
@@ -1,10 +1,36 @@
-def call(Map pushParms) {
-    def ecrLoginCmd = "aws ecr-public get-login-password --region ${pushParms.awsRegion} | docker login --username AWS --password-stdin ${pushParms.ecrUrl}"
-    ecrLoginCmd.execute()
+def pushToECR(imageTag, repositoryUrl, region) {
+    def ecrLoginCommand = "aws ecr-public get-login-password --region $region"
+    def dockerLoginCommand = "docker login --username AWS --password-stdin $repositoryUrl"
+    def dockerTagCommand = "docker tag arun_custom_repo:$imageTag $repositoryUrl/arun_custom_repo:$imageTag"
+    def dockerPushCommand = "docker push $repositoryUrl/arun_custom_repo:$imageTag"
 
-    def imageTagCmd = "docker tag ${pushParms.dockerimageTag} ${pushParms.ecrimageTag}"
-    imageTagCmd.execute()
+    def process = "${ecrLoginCommand} | ${dockerLoginCommand}".execute()
+    process.waitFor()
 
-    def ecrPushCmd = "docker push ${pushParms.ecrimageTag}"
-    ecrPushCmd.execute()
+    if (process.exitValue() == 0) {
+        // Successfully logged in to ECR
+        def tagProcess = dockerTagCommand.execute()
+        tagProcess.waitFor()
+
+        if (tagProcess.exitValue() == 0) {
+            // Image tagged successfully
+            def pushProcess = dockerPushCommand.execute()
+            pushProcess.waitFor()
+
+            if (pushProcess.exitValue() == 0) {
+                println "Image pushed to ECR successfully."
+            } else {
+                println "Failed to push image to ECR."
+                println "Error: ${pushProcess.err.text}"
+            }
+        } else {
+            println "Failed to tag the Docker image."
+            println "Error: ${tagProcess.err.text}"
+        }
+    } else {
+        println "Failed to login to ECR."
+        println "Error: ${process.err.text}"
+    }
 }
+
+
