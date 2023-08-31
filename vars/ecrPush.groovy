bbbf4b23a4bc149cc7fce7736094518e264bf37c
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-31 12:18:34 +0530

    Update ecrPush.groovy

--- vars/ecrPush.groovy
+++ vars/ecrPush.groovy
@@ -1,5 +1,22 @@
 def call() {
-  sh 'aws ecr-public get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_URL}'
-  sh 'docker tag ${DOCKER_IMAGE_TAG} ${ECR_IMAGE_TAG}'
-  sh 'docker push ${ECR_IMAGE_TAG}'
+  def ecrRepository = 'public.ecr.aws/c1l9p0p2'
+  def imageName = 'arun_custom_repo'
+  def imageTag = 'latest'
+  
+  dockerLogin(ecrRepository)
+  dockerTagAndPush(imageName, ecrRepository, imageTag)
 }
+
+def dockerLogin(repository) {
+  def ecrLoginCommand = "aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin $repository"
+  sh ecrLoginCommand
+}
+
+def dockerTagAndPush(imageName, repository, imageTag) {
+  def sourceImage = "$imageName:$imageTag"
+  def targetImage = "$repository/$imageName:$imageTag"
+  
+  sh "docker tag $sourceImage $targetImage"
+  sh "docker push $targetImage"
+}
+
