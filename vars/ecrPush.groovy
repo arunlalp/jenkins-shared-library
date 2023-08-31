62e78f92c57669ac970562d23ef324f1016a8c89
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-31 11:20:54 +0530

    Update ecrPush.groovy

--- vars/ecrPush.groovy
+++ vars/ecrPush.groovy
@@ -1,25 +1,10 @@
 def call(Map pushParms) {
     def ecrLoginCmd = "aws ecr-public get-login-password --region ${pushParms.awsRegion} | docker login --username AWS --password-stdin ${pushParms.ecrUrl}"
-    def ecrLoginOutput = ecrLoginCmd.execute()
-    if (ecrLoginOutput.text) {
-        println "ECR login successful."
-    } else {
-        println "ECR login failed."
-    }
+    ecrLoginCmd.execute()
 
     def imageTagCmd = "docker tag ${pushParms.dockerimageTag} ${pushParms.ecrimageTag}"
-    def imageTagOutput = imageTagCmd.execute()
-    if (imageTagOutput.text) {
-        println "Image tagging successful."
-    } else {
-        println "Image tagging failed."
-    }
+    imageTagCmd.execute()
 
     def ecrPushCmd = "docker push ${pushParms.ecrimageTag}"
-    def ecrPushOutput = ecrPushCmd.execute()
-    if (ecrPushOutput.text) {
-        println "Image push to ECR successful."
-    } else {
-        println "Image push to ECR failed."
-    }
+    ecrPushCmd.execute()
 }
