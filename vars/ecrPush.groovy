9cef7943761c229007ace207ab1e3a079a8fcde7
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-01 09:17:27 +0530

    Update ecrPush.groovy

--- vars/ecrPush.groovy
+++ vars/ecrPush.groovy
@@ -2,13 +2,14 @@ def call(Map params) {
   def ecrRepository = params.ecrRepository 
   def imageName = params.imageName 
   def imageTag = params.imageTag 
+  def awsRegion = params.awsRegion
   
   dockerLogin(ecrRepository)
-  dockerTagAndPush(imageName, ecrRepository, imageTag)
+  dockerTagAndPush(imageName, ecrRepository, imageTag, awsRegion)
 }
 
 def dockerLogin(repository) {
-  def ecrLoginCommand = "aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin $repository"
+  def ecrLoginCommand = "aws ecr-public get-login-password --region $awsRegion | docker login --username AWS --password-stdin $repository"
   sh ecrLoginCommand
 }
 
