f20fb38ac889fd3565e505638ae2d12255daab5c
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-10 11:10:56 +0530

    Create terraformValidate.groovy

---
+++ vars/terraformValidate.groovy
@@ -0,0 +1,11 @@
+def call(Map params) {
+   def projectDirectory = params.projectDirectory
+
+   terraformValidate(projectDirectory)
+}
+
+def terraformValidate(directory) {
+   dir(directory) {
+      def terraformValidateCommand = "terraform validate"
+      sh terraformValidateCommand 
+}
