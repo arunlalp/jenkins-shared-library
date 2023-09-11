088b9169b50bb08704c9b0046fec8eb4df98a908
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-11 09:40:33 +0530

    Update tfLint.groovy

--- vars/tfLint.groovy
+++ vars/tfLint.groovy
@@ -1,21 +1,14 @@
 def call(Map params) {
-   def tflintConfigFile = libraryResource('tflint.hcl')
-   def variableFile = params.variableFile
-   def directory = params.directory // Add this line to define the 'directory' parameter
+   def projectDirectory = params.projectDirectory
 
-   tfLint(tflintConfigFile, variableFile, directory)
+   tfLint(projectDirectory)
 }
 
-def tfLint(directory, variableFile) {
-   dir(directory) {
-      // Copy the TFLint configuration file to the working directory
-      sh "cp ${tflintConfigFile} .tflint.hcl"
-
-      // Update the variable file path in the copied TFLint configuration
-      sh "sed -i 's|varfile = .*|varfile = [\"${variableFile}\"]|' .tflint.hcl"
-
-      // Run TFLint
-      sh 'tflint --init'
-      sh 'tflint --disable-rule=terraform_required_version --disable-rule=terraform_required_providers'
-   }
+def tfLint(project_dir) {
+   dir(project_dir) {
+      def tfInitCommand = 'tflint --init'
+      def tfLintCommand = 'tflint --disable-rule=terraform_required_version --disable-rule=terraform_required_providers'
+      sh tfInitCommand
+      sh tfLintCommand
+   }   
 }
