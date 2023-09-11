2e7a5f6b662cf23844324f344ddae5b5b5daf348
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-11 10:12:53 +0530

    Update tfLint.groovy

--- vars/tfLint.groovy
+++ vars/tfLint.groovy
@@ -6,20 +6,7 @@ def call(Map params) {
 
 def tfLint(project_dir) {
    dir(project_dir) {
-      def tflintConfig = """
-        config {
-            module = true
-            force = false
-            disabled_by_default = false
-            varfile = ["../../../vars/dev/ec2.tfvars"]
-        }
-
-        plugin "aws" {
-            enabled = true
-            version = "0.26.0"
-            source = "github.com/terraform-linters/tflint-ruleset-aws"
-        }
-      """
+      def tflintConfig = resources/tflint/tflint.hcl
       writeFile file: '.tflint.hcl', text: tflintConfig
 
       def tfInitCommand = 'tflint --init'
