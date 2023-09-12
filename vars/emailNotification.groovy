732d987713cfc5cabe9159f36e6e0d9b7898430e
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-12 18:10:38 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,9 +1,9 @@
 def call(Map config) {
-    def to = config.to ?: error("'to' parameter is required for sending the email.")
     def status = config.status ?: "Notification"
     def successSubject = config.successSubject ?: "${status} Success"
     def failureSubject = config.failureSubject ?: "${status} Failure"
     def body = config.body ?: "${status} notification message."
+    def to = config.to ?: 'arunsample555@gmail.com'
     def attachLog = config.attachLog ?: false
 
     def subject = currentBuild.resultIsBetterOrEqualTo("SUCCESS") ? successSubject : failureSubject
