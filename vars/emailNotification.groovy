2bd45e28a952b7edd8766359622c58dcd1590872
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-12 17:10:54 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,14 +1,32 @@
-def call(Map config) {
-    def subject = config.subject ?: "Terraform Pipeline Notification"
-    def body = config.body ?: "Terraform pipeline notification message."
-    def to = config.to ?: 'arunsample555@gmail.com'
-    def attachLog = config.attachLog ?: false
+def call(Map successConfig = [:], Map failureConfig = [:]) {
+    def successSubject = successConfig.subject ?: "Terraform Pipeline Success"
+    def successBody = successConfig.body ?: "The Terraform pipeline has successfully completed."
+    def successTo = successConfig.to ?: 'arunsample555@gmail.com'
+    def successAttachLog = successConfig.attachLog ?: false
 
-    emailext(
-        subject: subject,
-        body: body,
-        recipientProviders: [[$class: 'DevelopersRecipientProvider']],
-        to: to,
-        attachLog: attachLog,
-    )
+    def failureSubject = failureConfig.subject ?: "Terraform Pipeline Failed"
+    def failureBody = failureConfig.body ?: "The Terraform pipeline has failed. Please investigate."
+    def failureTo = failureConfig.to ?: 'arunsample555@gmail.com'
+    def failureAttachLog = failureConfig.attachLog ?: false
+
+    post {
+        success {
+            emailext(
+                subject: successSubject,
+                body: successBody,
+                recipientProviders: [[$class: 'DevelopersRecipientProvider']],
+                to: successTo,
+                attachLog: successAttachLog,
+            )
+        }
+        failure {
+            emailext(
+                subject: failureSubject,
+                body: failureBody,
+                recipientProviders: [[$class: 'DevelopersRecipientProvider']],
+                to: failureTo,
+                attachLog: failureAttachLog,
+            )
+        }
+    }
 }
