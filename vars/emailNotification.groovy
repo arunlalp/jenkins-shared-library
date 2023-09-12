c2cdaeded83c9990039ef8f587772e43b44ebe58
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-12 17:22:10 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,38 +1,14 @@
-def call(Map successConfig = [:], Map failureConfig = [:]) {
-    def defaultSubject = "Terraform Pipeline Notification"
-    def defaultTo = 'arunsample555@gmail.com'
-    def defaultAttachLog = false
+def call(Map config) {
+    def subject = config.subject ?: "Terraform Pipeline Notification"
+    def body = config.body ?: "Terraform pipeline notification message."
+    def to = config.to ?: 'arunsample555@gmail.com'
+    def attachLog = config.attachLog ?: false
 
-    // Success email configuration
-    def successSubject = successConfig.subject ?: defaultSubject
-    def successBody = successConfig.body ?: "Terraform pipeline has successfully completed."
-    def successTo = successConfig.to ?: defaultTo
-    def successAttachLog = successConfig.attachLog ?: defaultAttachLog
-
-    // Failure email configuration
-    def failureSubject = failureConfig.subject ?: defaultSubject
-    def failureBody = failureConfig.body ?: "Terraform pipeline has failed. Please investigate."
-    def failureTo = failureConfig.to ?: defaultTo
-    def failureAttachLog = failureConfig.attachLog ?: defaultAttachLog
-
-    post {
-        success {
-            emailext(
-                subject: successSubject,
-                body: successBody,
-                recipientProviders: [[$class: 'DevelopersRecipientProvider']],
-                to: successTo,
-                attachLog: successAttachLog,
-            )
-        }
-        failure {
-            emailext(
-                subject: failureSubject,
-                body: failureBody,
-                recipientProviders: [[$class: 'DevelopersRecipientProvider']],
-                to: failureTo,
-                attachLog: failureAttachLog,
-            )
-        }
-    }
+    emailext(
+        subject: subject,
+        body: body,
+        recipientProviders: [[$class: 'DevelopersRecipientProvider']],
+        to: to,
+        attachLog: attachLog,
+    )
 }
