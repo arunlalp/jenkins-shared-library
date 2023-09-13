c93cefbc7dcc264f47635bc1279abba596cf672c
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-13 11:27:53 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,6 +1,4 @@
-import hudson.FilePath
-
-def sendEmailNotification(String pipelineStatus, String recipientEmail) {
+def sendEmailNotification(String pipelineStatus, String recipientEmail, String consoleOutput) {
     def subject, body
 
     if (pipelineStatus == 'success') {
@@ -9,21 +7,15 @@ def sendEmailNotification(String pipelineStatus, String recipientEmail) {
     } else if (pipelineStatus == 'failure') {
         subject = "Pipeline Failed"
         body = "The pipeline has failed. Please investigate."
+
+        // Extract the line starting with 'FAILED for resource:' from console output
+        def failedLine = consoleOutput =~ /FAILED for resource:.+/
+        if (failedLine) {
+            body += "\n\nFailed Line: ${failedLine[0]}"
+        }
     } else {
         subject = "Pipeline Status: $pipelineStatus"
         body = "The pipeline is in an unknown status: $pipelineStatus"
-    }
-
-    // Search for the build.log file in the Jenkins workspace or project directory
-    def workspace = currentBuild.rawBuild.workspace
-    def buildLogFilePath = findBuildLog(workspace)
-
-    // Attach the extracted line to the email body if the build.log file was found
-    if (buildLogFilePath) {
-        def failedLine = extractFailedLine(buildLogFilePath)
-        if (failedLine) {
-            body += "\n\nFailed Line from build.log:\n$failedLine"
-        }
     }
 
     emailext(
@@ -33,35 +25,4 @@ def sendEmailNotification(String pipelineStatus, String recipientEmail) {
         to: recipientEmail,
         attachLog: true
     )
-}
-
-def findBuildLog(workspace) {
-    def logFilePath = null
-    try {
-        def workspaceFilePath = new FilePath(workspace)
-        workspaceFilePath.list().each { filePath ->
-            if (filePath.getName() == 'build.log') {
-                logFilePath = filePath.getRemote()
-                return false // Stop searching once the file is found
-            }
-        }
-    } catch (Exception e) {
-        println("Error searching for build.log: ${e.message}")
-    }
-    return logFilePath
-}
-
-def extractFailedLine(String filePath) {
-    def failedLine = null
-    try {
-        new File(filePath).eachLine { line ->
-            if (line.startsWith('FAILED for resource:')) {
-                failedLine = line
-                return false // Stop reading after finding the line
-            }
-        }
-    } catch (Exception e) {
-        println("Error reading build.log: ${e.message}")
-    }
-    return failedLine
 }
