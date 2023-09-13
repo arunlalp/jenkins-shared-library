c0bc41a2e44438e3ff5dc604ae814d4de7cba432
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-13 11:17:12 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,4 +1,6 @@
-def sendEmailNotification(log, pipelineStatus, recipientEmail) {
+import hudson.FilePath
+
+def sendEmailNotification(String pipelineStatus, String recipientEmail) {
     def subject, body
 
     if (pipelineStatus == 'success') {
@@ -6,28 +8,22 @@ def sendEmailNotification(log, pipelineStatus, recipientEmail) {
         body = "The pipeline has successfully completed."
     } else if (pipelineStatus == 'failure') {
         subject = "Pipeline Failed"
-        body = "The pipeline has failed. Please investigate.\n\n"
-        
-        // Check if the log contains "Failed checks: 2"
-        if (log.contains("Failed checks: 2")) {
-            // Split the log by "Check:" to extract individual checks
-            def checks = log.split("Check:")
-            
-            // Loop through the checks to find failed checks
-            checks.each { check ->
-                if (check.contains("FAILED for resource")) {
-                    // Extract the failed resource and guide information
-                    def failedResource = check.substringAfter("FAILED for resource:").trim()
-                    def guide = check.substringAfter("Guide:").trim()
-                    
-                    // Add the failed resource and guide information to the email body
-                    body += "FAILED for resource: $failedResource\nGuide: $guide\n\n"
-                }
-            }
-        }
+        body = "The pipeline has failed. Please investigate."
     } else {
         subject = "Pipeline Status: $pipelineStatus"
         body = "The pipeline is in an unknown status: $pipelineStatus"
+    }
+
+    // Search for the build.log file in the Jenkins workspace or project directory
+    def workspace = currentBuild.rawBuild.workspace
+    def buildLogFilePath = findBuildLog(workspace)
+
+    // Attach the extracted line to the email body if the build.log file was found
+    if (buildLogFilePath) {
+        def failedLine = extractFailedLine(buildLogFilePath)
+        if (failedLine) {
+            body += "\n\nFailed Line from build.log:\n$failedLine"
+        }
     }
 
     emailext(
@@ -37,4 +33,35 @@ def sendEmailNotification(log, pipelineStatus, recipientEmail) {
         to: recipientEmail,
         attachLog: true
     )
+}
+
+def findBuildLog(workspace) {
+    def logFilePath = null
+    try {
+        def workspaceFilePath = new FilePath(workspace)
+        workspaceFilePath.list().each { filePath ->
+            if (filePath.getName() == 'build.log') {
+                logFilePath = filePath.getRemote()
+                return false // Stop searching once the file is found
+            }
+        }
+    } catch (Exception e) {
+        println("Error searching for build.log: ${e.message}")
+    }
+    return logFilePath
+}
+
+def extractFailedLine(String filePath) {
+    def failedLine = null
+    try {
+        new File(filePath).eachLine { line ->
+            if (line.startsWith('FAILED for resource:')) {
+                failedLine = line
+                return false // Stop reading after finding the line
+            }
+        }
+    } catch (Exception e) {
+        println("Error reading build.log: ${e.message}")
+    }
+    return failedLine
 }
