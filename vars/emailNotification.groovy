424f3801f866ee97370a1a67887867f93449c1c3
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-13 10:08:51 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -6,7 +6,8 @@ def sendEmailNotification(String pipelineStatus, String recipientEmail) {
         body = "The pipeline has successfully completed."
     } else if (pipelineStatus == 'failure') {
         subject = "Pipeline Failed"
-        body = "The pipeline has failed. Please investigate."
+        // Automatically extract failure reason from the Jenkins build log
+        body = getFailureReason()
     } else {
         subject = "Pipeline Status: $pipelineStatus"
         body = "The pipeline is in an unknown status: $pipelineStatus"
@@ -20,3 +21,21 @@ def sendEmailNotification(String pipelineStatus, String recipientEmail) {
         attachLog: true
     )
 }
+
+def getFailureReason() {
+    def buildLog = currentBuild.rawBuild.getLog(1000)
+    // Extract the failure reason from the log
+    def failureReason = buildLog.readLines().findAll { line ->
+        // Add your logic to find the failure reason in the log
+        // Example: return line.contains("FailureKeyword")
+    }.join('\n')
+
+    if (failureReason.isEmpty()) {
+        return "The reason for failure could not be determined."
+    } else {
+        return "The pipeline has failed with the following reason:\n${failureReason}"
+    }
+}
+
+// Example usage:
+// sendEmailNotification('failure', 'recipient@example.com')
