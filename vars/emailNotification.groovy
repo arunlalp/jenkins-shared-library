2e27255f274bb7287f6aa9a211200b2bac83ec17
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-13 11:48:00 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -12,21 +12,17 @@ def sendEmailNotification(String pipelineStatus, String recipientEmail) {
         body = "The pipeline is in an unknown status: $pipelineStatus"
     }
 
-    def logFilePath = '/var/lib/jenkins/consolelog.txt'
-    def logFile = new File(logFilePath)
-    logFile.text = '' // Clear the log file if it exists
-
-    // Redirect console output to both the console and the log file
-    def consoleOutput = new StringWriter()
-    System.setOut(new PrintStream(new TeeOutputStream(System.out, logFile.outputStream())))
-    System.setErr(new PrintStream(new TeeOutputStream(System.err, logFile.outputStream())))
-
     emailext(
         subject: subject,
         body: body,
         recipientProviders: [[$class: 'DevelopersRecipientProvider']],
         to: recipientEmail,
-        attachLog: false // Set this to false because we are manually redirecting the console output
+        attachLog: false // We won't attach the log here
     )
-}
 
+    // Capture the console output and save it to a file
+    script {
+        def consoleLog = currentBuild.rawBuild.getLog(1000)
+        writeFile file: '/var/lib/jenkins/consolelog.txt', text: consoleLog
+    }
+}
