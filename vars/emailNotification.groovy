306a08cf4c77b41e6698aba11c45ee6c8fbe3ccc
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-13 10:26:34 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,13 +1,16 @@
 def sendEmailNotification(String pipelineStatus, String recipientEmail) {
     def subject, body
 
-    if (pipelineStatus == 'success') {
+    if (pipelineStatus == 'SUCCESS') {
         subject = "Pipeline Success"
         body = "The pipeline has successfully completed."
-    } else if (pipelineStatus == 'failure') {
+    } else if (pipelineStatus == 'FAILURE') {
+        // Extract the failure reason from the pipeline log
+        def buildLog = currentBuild.rawBuild.getLog(1000)
+        def failureReason = extractFailureReason(buildLog)
+
         subject = "Pipeline Failed"
-        // Automatically extract failure reason from the Jenkins build log
-        body = getFailureReason()
+        body = "The pipeline has failed with the following reason:\n\n$failureReason\n\nPlease investigate."
     } else {
         subject = "Pipeline Status: $pipelineStatus"
         body = "The pipeline is in an unknown status: $pipelineStatus"
@@ -22,20 +25,24 @@ def sendEmailNotification(String pipelineStatus, String recipientEmail) {
     )
 }
 
-def getFailureReason() {
-    def buildLog = currentBuild.rawBuild.getLog(1000)
-    // Extract the failure reason from the log
-    def failureReason = buildLog.readLines().findAll { line ->
-        // Add your logic to find the failure reason in the log
-        // Example: return line.contains("FailureKeyword")
-    }.join('\n')
+def extractFailureReason(log) {
+    def failureReason = ""
+    def logLines = log.readLines()
 
-    if (failureReason.isEmpty()) {
-        return "The reason for failure could not be determined."
+    // Search for "Failed checks: 0" in the log
+    if (logLines.find { it.contains("Failed checks: 0") }) {
+        failureReason = "No check failures detected."
     } else {
-        return "The pipeline has failed with the following reason:\n${failureReason}"
-    }
-}
+        // Search for "FAILED for resource:" and "Guide:" in the log
+        def resourceFailed = logLines.find { it.contains("FAILED for resource:") }
+        def guide = logLines.find { it.contains("Guide:") }
 
-// Example usage:
-// sendEmailNotification('failure', 'recipient@example.com')
+        if (resourceFailed && guide) {
+            failureReason = "Resource Failed: $resourceFailed\nGuide: $guide"
+        } else {
+            failureReason = "Unknown failure reason. Please check the pipeline log."
+        }
+    }
+
+    return failureReason
+}
