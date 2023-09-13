5b81eaf8664352f1aaf71289174c0f88aeb3d150
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-13 18:42:11 +0530

    Update checkovScan.groovy

--- vars/checkovScan.groovy
+++ vars/checkovScan.groovy
@@ -4,11 +4,11 @@ def call(Map params) {
    def customPolicy = params.customPolicy
 
       def checkovPolicy = libraryResource 'checkov_policy/SecurityGroupInboundCIDR.py'
-      writeFile file: '/home/ubuntu/custom_checks/SecurityGroupInboundCIDR.py', text: checkovPolicy
+      writeFile file: '/home/ubuntu/SecurityGroupInboundCIDR.py', text: checkovPolicy
       def checkovInit = libraryResource 'checkov_policy/__init__.py'
-      writeFile file: '/home/ubuntu/custom_checks/__init__.py', text: checkovInit
+      writeFile file: '/home/ubuntu__init__.py', text: checkovInit
    
-   def checkovScanCommand = "checkov -f $project_dir/$plan_file_json --external-checks-dir $external_check_dir --check '/home/ubuntu/custom_checks' --hard-fail-on $custom_policy"
+   def checkovScanCommand = "checkov -f $project_dir/$plan_file_json --external-checks-dir $external_check_dir --check '/home/ubuntu' --hard-fail-on $custom_policy"
    sh checkovScanCommand   
 }
 
