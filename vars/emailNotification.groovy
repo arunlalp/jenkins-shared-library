9de72836adc738ce61e68b3e6068282116e2d541
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-13 14:40:25 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,31 +1,12 @@
 def sendEmailNotification(String pipelineStatus, String recipientEmail) {
-    def subject, body, consoleLog
+    def subject, body
 
     if (pipelineStatus == 'success') {
-        subject = "Pipeline Success"
+        subject = "Pipeline Success: ${PROJECT_NAME} - ${BUILD_NUMBER}"
         body = "The pipeline has successfully completed."
     } else if (pipelineStatus == 'failure') {
         subject = "Pipeline Failed"
         body = "The pipeline has failed. Please investigate."
-
-        // Capture the console output
-        script {
-            consoleLog = currentBuild.rawBuild.getLog(1000)
-        }
-
-        // Find the line starting with 'FAILED for resource:'
-        def failedLineIndex = -1
-        consoleLog.eachWithIndex { line, index ->
-            if (line.startsWith('FAILED for resource:')) {
-                failedLineIndex = index
-            }
-        }
-
-        // If a 'FAILED for resource:' line is found, grab the line starting with 'Guide:'
-        if (failedLineIndex != -1 && failedLineIndex + 1 < consoleLog.size()) {
-            def guideLine = consoleLog[failedLineIndex + 1]
-            body += "\n\nConsole Log (Line starting with 'Guide:'):\n$guideLine"
-        }
     } else {
         subject = "Pipeline Status: $pipelineStatus"
         body = "The pipeline is in an unknown status: $pipelineStatus"
@@ -36,9 +17,6 @@ def sendEmailNotification(String pipelineStatus, String recipientEmail) {
         body: body,
         recipientProviders: [[$class: 'DevelopersRecipientProvider']],
         to: recipientEmail,
-        attachLog: false // We won't attach the full log here
+        attachLog: true
     )
-
-    // Save the console output to a file
-    writeFile file: '/var/lib/jenkins/consolelog.txt', text: consoleLog
 }
