49451ba4686789d3728d9f10dbe45722ec752c97
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-14 11:35:36 +0530

    Update checkovScan.groovy

--- vars/checkovScan.groovy
+++ vars/checkovScan.groovy
@@ -19,17 +19,9 @@ def call(Map params) {
 }
 
 def checkovScan(project_dir, plan_file_json, custom_policy, checkov_policy_dir) {
-    def checkovReportFile = "${env.WORKSPACE}/checkov-report.html"
-    def checkovScanCommand = "checkov -f $project_dir/$plan_file_json --external-checks-dir $checkov_policy_dir --check $custom_policy --hard-fail-on $custom_policy --output-file-path $checkovReportFile --output html"
+    def checkovScanCommand = "checkov -f $project_dir/$plan_file_json --external-checks-dir $checkov_policy_dir --check $custom_policy --hard-fail-on $custom_policy > checkov-report.html"
     sh checkovScanCommand
 
-    // Copy the HTML report to the workspace
-    copyArtifacts(
-        filter: 'checkov-report.html', 
-        fingerprintArtifacts: true, 
-        projectName: env.JOB_NAME, 
-        selector: specific("${env.BUILD_NUMBER}")
-    )
 }
 
 
