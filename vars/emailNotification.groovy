8e85f150f6fedd93c51a78eafae073d927cdd34a
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-14 13:43:42 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,116 +1,22 @@
 def sendEmailNotification(String pipelineStatus, String recipientEmail) {
+    def buildNotification = libraryResource 'notification/notify.tpl'
     def subject, body
 
     if (pipelineStatus == 'success') {
-        subject = "[$JOB_NAME] Build #${BUILD_NUMBER} - Pipeline Success"
-        body = """<html>
-                    <head>
-                        <style>
-                            /* Add your CSS styling here */
-                            body {
-                                font-family: Arial, sans-serif;
-                                background-color: #f0f0f0;
-                            }
-                            .header {
-                                background-color: #007bff;
-                                color: #ffffff;
-                                padding: 10px;
-                                text-align: center;
-                            }
-                            .content {
-                                padding: 20px;
-                            }
-                            a {
-                                color: #007bff;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <div class="header">
-                            <h1>Pipeline Success</h1>
-                        </div>
-                        <div class="content">
-                            <p>Your pipeline has successfully completed.</p>
-                            <p>Click <a href="${BUILD_URL}">here</a> to view the build details.</p>
-                        </div>
-                    </body>
-                </html>"""
+        subject = "Terraform Pipeline Success"
+        body = "${buildNotification}"
     } else if (pipelineStatus == 'failure') {
-        subject = subject = "[$JOB_NAME] Build #${BUILD_NUMBER} - Pipeline Failed"
-        body = """<html>
-                    <head>
-                        <style>
-                            /* Add your CSS styling here */
-                            body {
-                                font-family: Arial, sans-serif;
-                                background-color: #f0f0f0;
-                            }
-                            .header {
-                                background-color: #ff0000;
-                                color: #ffffff;
-                                padding: 10px;
-                                text-align: center;
-                            }
-                            .content {
-                                padding: 20px;
-                            }
-                            a {
-                                color: #007bff;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <div class="header">
-                            <h1>Pipeline Failed</h1>
-                        </div>
-                        <div class="content">
-                            <p>Your pipeline has failed. Please investigate.</p>
-                            <p>Click <a href="${BUILD_URL}">here</a> to view the build details.</p>
-                        </div>
-                    </body>
-                </html>"""
+        subject = "Terraform Pipeline Failed"
+        body = "${buildNotification}"
     } else {
-        subject = "[$JOB_NAME] Build #${BUILD_NUMBER} - Pipeline Status: $pipelineStatus"
-        body = """<html>
-                    <head>
-                        <style>
-                            /* Add your CSS styling here */
-                            body {
-                                font-family: Arial, sans-serif;
-                                background-color: #f0f0f0;
-                            }
-                            .header {
-                                background-color: #333;
-                                color: #ffffff;
-                                padding: 10px;
-                                text-align: center;
-                            }
-                            .content {
-                                padding: 20px;
-                            }
-                            a {
-                                color: #007bff;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <div class="header">
-                            <h1>Pipeline Status: $pipelineStatus</h1>
-                        </div>
-                        <div class="content">
-                            <p>Your pipeline is in an unknown status: $pipelineStatus</p>
-                            <p>Click <a href="${BUILD_URL}">here</a> to view the build details.</p>
-                        </div>
-                    </body>
-                </html>"""
+        subject = "Terraform Pipeline Status: $pipelineStatus"
+        body = "The Terraform pipeline is in an unknown status: $pipelineStatus"
     }
 
     emailext(
         subject: subject,
         body: body,
-        recipientProviders: [[$class: 'DevelopersRecipientProvider']],
-        to: recipientEmail,
-        mimeType: 'text/html', // Set the content type to HTML
-        attachLog: true
+        recipientProviders: [[$class: 'CulpritsRecipientProvider']],
+        to: recipientEmail
     )
 }
