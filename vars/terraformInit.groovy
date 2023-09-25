a3ec4b726e42ce82d44983d1042b7545ab425024
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-25 14:35:53 +0530

    Update terraformInit.groovy

--- vars/terraformInit.groovy
+++ vars/terraformInit.groovy
@@ -6,14 +6,14 @@ def call(Map params) {
 }
 
 def terraformInit(project_dir, var_file) {
-   dir(project_dir) {
+   dir(project_dir, var_file) {
       def terraformInitCommand = """
         terraform init \\
           -backend-config="key=dev/jenkins_agent.tfstate" \\
           -backend-config="bucket=dcube-terraform-state" \\
           -backend-config="region=us-west-2" \\
           -backend-config="dynamodb_table=terraform-state-lock" \\
-          -var-file=../../vars/infra/dev/jenkins-agent.tfvars
+          -var-file=../../vars/infra/dev/$var_file
       """
 
       sh terraformInitCommand
