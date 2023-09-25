e72663d14cbfab79db6cebc0dc304f11600a03d5
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-25 13:45:20 +0530

    Update terraformInit.groovy

--- vars/terraformInit.groovy
+++ vars/terraformInit.groovy
@@ -7,14 +7,14 @@ def call(Map params) {
 
 def terraformInit(project_dir, var_file) {
    dir(project_dir, var_file) {
-      def terraformInitCommand = """
+      def terraformInitCommand = "
          terraform init \
          -backend-config="key=dev/vpc.tfstate" \
          -backend-config="bucket=dcube-terraform-state" \
          -backend-config="region=us-west-2" \
          -backend-config="dynamodb_table=terraform-state-lock" \
          -var-file=../../vars/infra/dev/${var_file}
-      """
+      "
       sh terraformInitCommand.trim()
    }
 }
